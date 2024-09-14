d5787057160066d7862f3502a27ad96912f145df
Roman Gerasimov <reflechant@users.noreply.github.com>
2024-09-14 16:00:11 +0200

    use bigger samples for benchmarks

--- bench_test.go
+++ bench_test.go
@@ -5,9 +5,9 @@ import (
 )
 
 func BenchmarkKeys(b *testing.B) {
-	n := &Node[int, string]{10000, "", nil, nil}
-	for i := range 10 {
-		n.Insert(2048+i, "")
+	n := &Node[int, string]{50000, "", nil, nil}
+	for i := range 100000 {
+		n.Insert(i, "")
 	}
 	b.ResetTimer()
 	var sum int
@@ -22,9 +22,9 @@ func BenchmarkKeys(b *testing.B) {
 var q int
 
 func BenchmarkKeys2(b *testing.B) {
-	n := &Node[int, string]{10000, "", nil, nil}
-	for i := range 10 {
-		n.Insert(2048+i, "")
+	n := &Node[int, string]{50000, "", nil, nil}
+	for i := range 100000 {
+		n.Insert(i, "")
 	}
 	b.ResetTimer()
 	var sum int
